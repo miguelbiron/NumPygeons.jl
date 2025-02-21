6ac0f6e6d5bab33288edd72461242498c020cd02
Miguel Biron-Lattes <miguelbl9@gmail.com>
2025-02-20 16:35:43 -0800

    init

--- src/NumPygeons.jl
+++ src/NumPygeons.jl
@@ -1,19 +1,31 @@
 module NumPygeons
 
+using DocStringExtensions
 using Pigeons
 using PythonCall
 
-# initialization (handles precompilation correctly)
+# import python packages
+# this approach handles precompilation correctly
 const autostep = PythonCall.pynew()
+const jax = PythonCall.pynew()
 const numpyro = PythonCall.pynew()
 const bridge = PythonCall.pynew()
 function __init__()
     PythonCall.pycopy!(autostep, pyimport("autostep"))
+    PythonCall.pycopy!(jax, pyimport("jax"))
     PythonCall.pycopy!(numpyro, pyimport("numpyro"))
     PythonCall.pycopy!(bridge, pyimport("numpygeons").bridge)
+    return
 end
 
-export NumPyroPath
+"""
+$SIGNATURES
+
+Make a JAX random number generator (RNG) key from a Julia RNG.
+"""
+jax_rng_key(rng) = jax.random.key(pyint(Pigeons.java_seed(rng)))
+
+export NumPyroPath, NumPyroExplorer
 include("interface.jl")
 
 end # module NumPygeons
--- src/interface.jl
+++ src/interface.jl
@@ -1,7 +1,7 @@
 """
 $SIGNATURES
 
-A thin wrapper around a NumPyro model to provide a linear prior-posterior path.
+Provides a linear prior-posterior path for NumPyro models.
 
 $FIELDS
 """
@@ -35,7 +35,7 @@ Create a [`NumPyroPath`](@ref) from model arguments. Note: following the
 NumPyro convention, the model itself should be passed to the kernel inside
 the explorer.
 """
-function NumPyroPath(model_args::Py, model_kwargs::Py)
+function NumPyroPath(;model_args::Py = pytuple(()), model_kwargs::Py = pydict())
     @assert model_args isa Py && pyisinstance(model_args, pytype(pytuple(()))),
         "`model_args` should be a python tuple."
     @assert model_kwargs isa Py && pyisinstance(model_kwargs, pytype(pydict())),
@@ -50,13 +50,19 @@ end
 # Update the fields in the path using the explorer
 function Pigeons.create_path(
     path::NumPyroPath,
-    inp::Inputs{<:Any,<:Any,NumPyroExplorer}
+    inp::Inputs{NumPyroPath,<:Any,NumPyroExplorer}
     )
+    # check we have a valid NumPyro MCMC kernel
+    # note: kernel is later initialized in `Pigeons.initialization`
     kernel = inp.explorer.kernel
     @assert kernel isa Py && pyisinstance(kernel, numpyro.infer.MCMC)
+
+    # make interpolator function
     interpolator = bridge.make_interpolator(
         kernel.model, path.model_args, path.model_kwargs
     )
+
+    # update path fields and return
     PythonCall.pycopy!(path.interpolator, interpolator)
     PythonCall.pycopy!(path.kernel, kernel)
     return path
@@ -73,7 +79,6 @@ Pigeons.default_explorer(::NumPyroPath) = throw(ArgumentError(
     "No explorer found. Use a `NumPyroExplorer`."
 ))
 
-
 """
 $SIGNATURES
 
@@ -86,6 +91,26 @@ struct NumPyroState
     Full state of the MCMC kernel used within NumPyro.
     """
     kernel_state::Py
+end
+
+# state initialization
+# note: this occurs *after* the `Pigeons.Shared` object is created, and therefore
+# after `Pigeons.create_path` is called.
+function Pigeons.initialization(
+    inp::Inputs{NumPyroPath,<:Any,NumPyroExplorer},
+    replica_rng, 
+    replica_idx
+    )
+    path = inp.target
+    kernel = inp.explorer.kernel
+    initial_kernel_state = kernel.init(
+        jax_rng_key(replica_rng), 
+        pyint(0), 
+        pybuiltins.None, 
+        path.model_args, 
+        path.model_kwargs
+    )
+    return initial_kernel_state
 end
 
 
@@ -107,12 +132,16 @@ struct NumPyroLogPotential
     Note: Following the NumPyro convention, a potential is the negative of
     a Pigeons logpotential.    
     """
-    potential::Py
+    potential_fn::Py
 end
 
 Pigeons.interpolate(path::NumPyroPath, beta::Real) = 
     NumPyroLogPotential(path.kernel, path.interpolator(pyfloat(beta)))
 
-function (lp::NumPyroLogPotential)(x::NumPyroState)
-    # TODO
+function (log_potential::NumPyroLogPotential)(state::NumPyroState)
+    kernel = log_potential.kernel
+    potential_fn = log_potential.potential_fn
+    kernel_state = state.kernel_state
+    unconstrained_sample = pygetattr(kernel_state, kernel.sample_field)
+    return -pyconvert(Float64, potential_fn(unconstrained_sample))
 end
